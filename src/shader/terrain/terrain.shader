052565d9fc3a820535dadefbdc851c0888ebd127
bbielsa <bgb7@njit.edu>
2019-01-06 18:01:09 -0500

    Rendering flat world

--- src/shader/terrain/terrain.shader
+++ src/shader/terrain/terrain.shader
@@ -1,13 +1,26 @@
 shader_type spatial;
 
+uniform sampler2D height_map;
+
 void vertex() {
+	
+	
 	if(VERTEX.y > 0.0) {
-		VERTEX.y = max(0, sin(TIME + VERTEX.x + VERTEX.z));	
+		float height = texture(height_map, UV).r * 256.0;
+		
+		VERTEX.y = height;
+		
 	}
 }
 
 void fragment() {	
 	NORMAL = normalize(cross(dFdx(VERTEX), dFdy(VERTEX)));
-	// green color
+
 	ALBEDO = vec3(39.0 / 256.0, 174.0 / 256.0, 96.0 / 256.0);
+
+	//ALBEDO = vec3(UV.x, 0, UV.y);
+
+//	if(UV.x < 1.0){
+//		ALBEDO *= 0.8;
+//	}
 }
--- src/terrain/terrain.gd
+++ src/terrain/terrain.gd
@@ -7,21 +7,33 @@ onready var terrain_shader = mesh.material
 
 func _ready():
 	terrain_model.connect("terrain_changed", self, "_terrain_changed")
-	
-	terrain_model.deform(0, 0, 1)
-	terrain_model.deform(4, 4, 2)
-	terrain_model.deform(4, 4, -2)
+	terrain_model.deform(4, 4, 1)
+
 	terrain_model.print_map()
 	
 func _terrain_changed(height_map):
 	print("_terrain_changed called, generating data texture")
 	
 	var height_map_texture = generate_height_map_texture(height_map)
+	var shader = terrain_shader.shader
+	
+	set_texture_param(shader, "height_map", height_map_texture)
+	
+func set_texture_param(shader, name, texture):
+	shader.set_default_texture_param(name, texture)	
 	
 func height_to_pixel(height):
 	return Color(height / 255.0, 0, 0)
 	
 func generate_height_map_texture(height_map):
+	var image_data = generate_height_map_image_data(height_map)
+	var texture = ImageTexture.new()
+	
+	texture.create_from_image(image_data, 0)
+
+	return texture
+	
+func generate_height_map_image_data(height_map):
 	var map_dimension_vertices = terrain_model.map_dimension + 1
 	var image_data = Image.new()
 	
