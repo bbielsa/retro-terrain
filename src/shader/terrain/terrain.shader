9eac75f4b9d092865fae12a4b6fc6c491d69ab75
bbielsa <bgb7@njit.edu>
2019-01-13 19:26:53 -0500

    Loading heightmap data from texture. Switched to SurfaceTool to generate the plane allowing me to set the UVs which I can use to draw the grid

--- src/shader/terrain/terrain.shader
+++ src/shader/terrain/terrain.shader
@@ -1,26 +1,35 @@
 shader_type spatial;
 
 uniform sampler2D height_map;
+uniform sampler2D grass_texture;
 
 void vertex() {
-	
-	
-	if(VERTEX.y > 0.0) {
-		float height = texture(height_map, UV).r * 256.0;
-		
-		VERTEX.y = height;
-		
-	}
+		float height = texture(height_map, VERTEX.xz / 10.0).r;
+
+//		VERTEX.y = height;
+
+//	if(VERTEX.x == 4.0 && VERTEX.z == 4.0)
+//		VERTEX.y = 1.0;
+
+	VERTEX.y += height * 255.0;
 }
 
 void fragment() {	
 	NORMAL = normalize(cross(dFdx(VERTEX), dFdy(VERTEX)));
 
-	ALBEDO = vec3(39.0 / 256.0, 174.0 / 256.0, 96.0 / 256.0);
-
-	//ALBEDO = vec3(UV.x, 0, UV.y);
-
-//	if(UV.x < 1.0){
-//		ALBEDO *= 0.8;
-//	}
+//	ALBEDO = texture(height_map, VERTEX.xz).rgb;
+	ALBEDO = vec3(1.0);
+//	ALBEDO = vec3(39.0 / 256.0, 174.0 / 256.0, 96.0 / 256.0);
+//	ALBEDO = vec3(UV.x, 1.0, UV.y);
+//	if (UV.x > 0.9)
+//		ALBEDO = vec3(0.0);	
+	if (UV2.x < 0.04) {
+		ALBEDO *= 0.8;
+	}
 }
+
+
+//	vec2 texture_pos = vec2(mod(VERTEX.x, 32.0), mod(VERTEX.z, 32.0));
+//	vec2 texture_pos = vec2(0);
+//	ALBEDO = texture(grass_texture, UV).rgb;
+
--- src/terrain/terrain.gd
+++ src/terrain/terrain.gd
@@ -3,19 +3,76 @@ extends MeshInstance
 export(ShaderMaterial) var terrain_material = null
 
 onready var terrain_model = get_node("TerrainModel")
-onready var terrain_shader = mesh.material
+var terrain_shader
 
 func _ready():
+	generate_terrain()
+	
 	terrain_model.connect("terrain_changed", self, "_terrain_changed")
-	terrain_model.deform(4, 4, 1)
+	terrain_model.deform(1, 1, 1)
 
 	terrain_model.print_map()
+	
+func generate_terrain():
+	var surface = SurfaceTool.new()
+	
+	surface.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	for y in range(terrain_model.map_dimension):
+		for x in range(terrain_model.map_dimension):
+			add_tile(surface, x, y)
+			
+	surface.generate_normals()
+	surface.index()
+	surface.set_material(terrain_material)
+	
+	set_mesh(surface.commit())
+	
+func add_tile(surface, x, y):
+	var offset = Vector3(x, 0, y)
+	
+	var a = offset
+	var b = offset + Vector3(1, 0, 0)
+	var c = offset + Vector3(1, 0, 1)
+	var d = offset + Vector3(0, 0, 1)
+	var e = offset + Vector3(0.5, 0, 0.5)
+	
+	var outer = Vector2(0, 0)
+	var inner = Vector2(1, 1)
+	
+#	abe, bce, cde, dae
+	surface.add_uv2(outer)
+	surface.add_vertex(a)
+	surface.add_vertex(b)
+	surface.add_uv2(inner)
+	surface.add_vertex(e)
+	
+#	surface.add_uv(Vector2(1, 0))
+	surface.add_uv2(outer)
+	surface.add_vertex(b)
+	surface.add_vertex(c)
+	surface.add_uv2(inner)
+	surface.add_vertex(e)
+	
+#	surface.add_uv(Vector2(0, 1))
+	surface.add_uv2(outer)
+	surface.add_vertex(c)
+	surface.add_vertex(d)
+	surface.add_uv2(inner)
+	surface.add_vertex(e)
+	
+#	surface.add_uv(Vector2(1, 1))
+	surface.add_uv2(outer)
+	surface.add_vertex(d)
+	surface.add_vertex(a)
+	surface.add_uv2(inner)
+	surface.add_vertex(e)
 	
 func _terrain_changed(height_map):
 	print("_terrain_changed called, generating data texture")
 	
 	var height_map_texture = generate_height_map_texture(height_map)
-	var shader = terrain_shader.shader
+	var shader = terrain_material.shader
 	
 	set_texture_param(shader, "height_map", height_map_texture)
 	
